027c2ccd8513e4f02ff04f7c839115c9a8448eb8
Peter Szilvasi <peti.szilvasi95@gmail.com>
2023-06-26 21:02:14 +0200

    Add control nodes for player movement

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -5,12 +5,8 @@ var moves = 0
 
 var grid_size = 16
 
-func _unhandled_input(event):
-	if event.is_action_pressed("ui_accept"):
-		$Player.move('ui_down')
-
 func _process(delta):
-	$MovesLabel.text = 'Moves: ' + str(moves)
+	$GameInfo/MovesLabel.text = 'Moves: ' + str(moves)
 	if game_end == false:
 		var spots = $Spots.get_child_count()
 		for spot in $Spots.get_children():
@@ -18,7 +14,7 @@ func _process(delta):
 				spots -= 1
 				
 		if spots == 0:
-			$AcceptDialog.popup()
+			$GameInfo/AcceptDialog.popup()
 			game_end = true
 
 
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,6 +10,13 @@ var inputs = {
 	'ui_right': Vector2.RIGHT
 }
 
+var directions = {
+	0: 'ui_up',
+	1: 'ui_down',
+	2: 'ui_left',
+	3: 'ui_right'
+}
+
 func _unhandled_input(event):
 	for direction in inputs.keys():
 		if event.is_action_pressed(direction):
@@ -33,3 +40,19 @@ func move(direction):
 			if collider.move(direction):
 				position += vector_pos
 				game.moves += 1
+
+
+func _on_Button_pressed():
+	var line_edit = get_parent().get_node("Algorithm/LineEdit")
+	var number_of_steps = int(line_edit.text)
+	
+	var direction_list = get_parent().get_node("Algorithm/DirectionList")
+	
+	# Get the first selected items because the single selection mode
+	var item_idx = direction_list.get_selected_items()[0]
+	var direction = directions[item_idx]
+	
+	for i in number_of_steps:
+		move(direction)
+		yield(get_tree().create_timer(0.5), "timeout")
+	
